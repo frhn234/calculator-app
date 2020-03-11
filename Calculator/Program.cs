b197564f65479479f72dba36a06f9f1413d4efe1
frhn <frhnag860@gmail.com>
2020-03-11 11:44:01 +0700

    inisialisasi project

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.Title = "Apliaction Calculator";
+
+            //int a = 10; //deklarasi nilai a, dengan nilai awal 10
+            //int b = 6;  //deklarasi nilai b, dengan nilai awal 6
+
+            Console.Write("Inputkan Nilai a = ");
+            int a = int.Parse(Console.ReadLine()); //proses casting
+
+            Console.Write("Inputkan Nilai b = ");
+            int b = int.Parse(Console.ReadLine()); //proses casting
+
+            Console.WriteLine(); //tambahkan baris kosong
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a* b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }   
+    }
+}
